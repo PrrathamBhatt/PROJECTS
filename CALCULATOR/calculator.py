0393cfb938f218cde42f5d5e8d6fa7980a2b4a00
PrrathamBhatt <prathambhatt780@gmail.com>
2026-08-18 16:58:46 +0530

    Implement basic calculator functionality in Python

---
+++ CALCULATOR/calculator.py
@@ -0,0 +1,28 @@
+print(" CALCULATOR ")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+
+elif operator == "-":
+    result = num1 - num2
+
+elif operator == "*":
+    result = num1 * num2
+
+elif operator == "/":
+    if num2 == 0:
+        print("Cannot divide by zero!")
+        result = None
+    else:
+        result = num1 / num2
+
+else:
+    print("Invalid operator!")
+    result = None
+
+if result is not None:
+    print("Result:", result)
