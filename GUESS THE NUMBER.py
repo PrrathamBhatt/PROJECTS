9c18ce798b9c45e8da7681b1fb04a09ef9cd045d
Pratham Bhatt <prathambhatt780@gmail.com>
2026-08-18 16:25:50 +0530

    First version of Guess The Number game

---
+++ GUESS THE NUMBER.py
@@ -0,0 +1,16 @@
+import random
+
+comp_num = random.randint(1, 100)
+
+print("Welcome Pratham ")
+
+while True:
+    guess = int(input("Enter your guess: "))
+
+    if guess < comp_num:
+        print("Too low! Try again.")
+    elif guess > comp_num:
+        print("Too high! Try again.")
+    else:
+        print("🎉 Congratulations! You guessed the correct number.🎉")
+        break
