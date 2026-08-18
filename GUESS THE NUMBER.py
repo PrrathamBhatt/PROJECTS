b7746bc9a8caad3f7328f674e3ec9f5bc2ef8a42
Pratham Bhatt <prathambhatt780@gmail.com>
2026-08-18 16:33:50 +0530

    UPDATE GUESS THE NUMBER GAME

--- GUESS THE NUMBER.py
+++ GUESS THE NUMBER.py
@@ -3,7 +3,7 @@ import random
 comp_num = random.randint(1, 100)
 
 print("Welcome Pratham ")
-
+print("Welcome to Guess the Number Game!")
 while True:
     guess = int(input("Enter your guess: "))
 
